836f5cfbce7d8fd6c51f60551a60e5641c5bf044
raamNnoodlez <ashirbutt998@gmail.com>
2023-09-25 16:38:40 +0200

    removed login consoles

--- login.php
+++ login.php
@@ -8,13 +8,13 @@
     $sql = "SELECT * FROM Users;";
     $result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
+    // if ($result->num_rows > 0) {
+    //     while($row = $result->fetch_assoc()) {
+    //         echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+    //     }
+    // } else {
+    //     echo "0 results";
+    // }
 
     $conn->close();
 ?>
