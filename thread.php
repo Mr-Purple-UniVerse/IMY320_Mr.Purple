977e93a47d00869720bd8d8392b4d9b6cc597e29
DianLahouter <lahouterd@gmail.com>
2023-10-16 10:51:48 +0200

    Added favicon

--- thread.php
+++ thread.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thread</title>
     <link rel="stylesheet" href="CSS/thread.css">
+    <link rel="icon" href="./assets/favicon.ico">
 </head>
 <body>
 <div class="circle"></div>
